95a65fb90a5eebd7a6e80de48a62de25958df907
Giovanni Gatti Pinheiro <ggattipinheiro@mediametrie.fr>
2015-09-29 18:04:13 +0200

    Restructuring + TODO sessions.

--- cloudapp-mp1/src/MP1.java
+++ cloudapp-mp1/src/MP1.java
@@ -7,6 +7,7 @@ import java.nio.file.FileSystems;
 import java.nio.file.Files;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
@@ -14,148 +15,148 @@ import java.util.Random;
 import java.util.Set;
 
 public class MP1 {
-	private static final int DEFAULT_SIZE = 1024;
-	Random generator;
-	String userName;
-	String inputFileName;
-	String delimiters = " \t,;.?!-:@[](){}_*/";
-	String[] stopWordsArray = { "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
-			"yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
-			"itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that",
-			"these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having",
-			"do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
-			"of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
-			"after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-			"further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
-			"few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
-			"too", "very", "s", "t", "can", "will", "just", "don", "should", "now" };
+    private static final int DEFAULT_SIZE = 1024;
+    Random generator;
+    String userName;
+    String inputFileName;
+    String delimiters = " \t,;.?!-:@[](){}_*/";
+    String[] stopWordsArray = { "i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you", "your", "yours",
+            "yourself", "yourselves", "he", "him", "his", "himself", "she", "her", "hers", "herself", "it", "its",
+            "itself", "they", "them", "their", "theirs", "themselves", "what", "which", "who", "whom", "this", "that",
+            "these", "those", "am", "is", "are", "was", "were", "be", "been", "being", "have", "has", "had", "having",
+            "do", "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until",
+            "while",
+            "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
+            "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
+            "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+            "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
+            "too", "very", "s", "t", "can", "will", "just", "don", "should", "now" };
 
-	private static class WordCounter {
-		private String word;
-		private int count = 0;
+    private static class Word {
+        private final String word;
 
-		private WordCounter(String word) {
-			this.word = word;
-		}
+        public Word(String word) {
+            this.word = word;
+        }
 
-		public String getWord() {
-			return word;
-		}
+        public String getWord() {
+            return word;
+        }
+    }
 
-		public int getCount() {
-			return count;
-		}
+    private static class Counter implements Comparator<Counter> {
+        private final Word word;
+        private int count = 1;
 
-		public void count() {
-			count++;
-		}
+        private Counter(Word word) {
+            this.word = word;
+        }
 
-		@Override
-		public int hashCode() {
-			final int prime = 31;
-			int result = 1;
-			result = prime * result + ((word == null) ? 0 : word.hashCode());
-			return result;
-		}
+        public Word getWord() {
+            return word;
+        }
 
-		@Override
-		public boolean equals(Object obj) {
-			if (this == obj)
-				return true;
-			if (obj == null)
-				return false;
-			if (getClass() != obj.getClass())
-				return false;
-			WordCounter other = (WordCounter) obj;
-			if (word == null) {
-				if (other.word != null)
-					return false;
-			} else if (!word.equals(other.word))
-				return false;
-			return true;
-		}
-	}
+        public int getCount() {
+            return count;
+        }
 
-	private static class SelectiveWordCounter {
-		private final Map<Integer, WordCounter> counter = new HashMap<Integer, WordCounter>();
+        public void count() {
+            count++;
+        }
 
-		public SelectiveWordCounter() {
-		}
+        @Override
+        public int compare(Counter o1, Counter o2) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+    }
 
-		public void addWordCount(String word) {
-			// if(counter.
-		}
-	}
+    private static class Register {
+        private final Map<Word, Counter> register = new HashMap<Word, Counter>();
 
-	void initialRandomGenerator(String seed) throws NoSuchAlgorithmException {
-		MessageDigest messageDigest = MessageDigest.getInstance("SHA");
-		messageDigest.update(seed.toLowerCase().trim().getBytes());
-		byte[] seedMD5 = messageDigest.digest();
+        public void count(Word word) {
+            if (register.containsKey(word)) {
+                register.get(word).count();
+            } else {
+                register.put(word, new Counter(word));
+            }
+        }
 
-		long longSeed = 0;
-		for (int i = 0; i < seedMD5.length; i++) {
-			longSeed += (seedMD5[i] & 0xffL) << (8 * i);
-		}
+        public void getOrdered() {
+            // Collections.sort(register.values());
+            // TODO
+        }
+    }
 
-		this.generator = new Random(longSeed);
-	}
+    void initialRandomGenerator(String seed) throws NoSuchAlgorithmException {
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA");
+        messageDigest.update(seed.toLowerCase().trim().getBytes());
+        byte[] seedMD5 = messageDigest.digest();
 
-	Integer[] getIndexes() throws NoSuchAlgorithmException {
-		Integer n = 10000;
-		Integer number_of_lines = 50000;
-		Integer[] ret = new Integer[n];
-		this.initialRandomGenerator(this.userName);
-		for (int i = 0; i < n; i++) {
-			ret[i] = generator.nextInt(number_of_lines);
-		}
-		return ret;
-	}
+        long longSeed = 0;
+        for (int i = 0; i < seedMD5.length; i++) {
+            longSeed += (seedMD5[i] & 0xffL) << (8 * i);
+        }
 
-	public MP1(String userName, String inputFileName) {
-		this.userName = userName;
-		this.inputFileName = inputFileName;
-	}
+        this.generator = new Random(longSeed);
+    }
 
-	public String[] process() throws Exception {
-		String[] ret = new String[20];
+    Integer[] getIndexes() throws NoSuchAlgorithmException {
+        Integer n = 10000;
+        Integer number_of_lines = 50000;
+        Integer[] ret = new Integer[n];
+        this.initialRandomGenerator(this.userName);
+        for (int i = 0; i < n; i++) {
+            ret[i] = generator.nextInt(number_of_lines);
+        }
+        return ret;
+    }
 
-		String contents = readFile(inputFileName);
+    public MP1(String userName, String inputFileName) {
+        this.userName = userName;
+        this.inputFileName = inputFileName;
+    }
 
-		Set<WordCounter> selectiveWordCounter = new HashSet<WordCounter>();
+    public String[] process() throws Exception {
+        String[] ret = new String[20];
 
-		return ret;
-	}
+        String contents = readFile(inputFileName);
 
-	private String readFile(String inputFileName) throws IOException {
-		File file = new File(inputFileName);
-		if (!file.exists() || !file.isFile() || !file.canRead()) {
-			throw new UnsupportedOperationException("It can only process readable text files");
-		}
+        Set<Counter> selectiveWordCounter = new HashSet<Counter>();
 
-		StringBuilder contents = new StringBuilder(DEFAULT_SIZE);
+        return ret;
+    }
 
-		try (InputStream in = Files.newInputStream(FileSystems.getDefault().getPath(file.getPath()));
-				BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
-			String line = null;
-			while ((line = reader.readLine()) != null) {
-				contents.append(line + "\n");
-			}
-		}
+    private String readFile(String inputFileName) throws IOException {
+        File file = new File(inputFileName);
+        if (!file.exists() || !file.isFile() || !file.canRead()) {
+            throw new UnsupportedOperationException("It can only process readable text files");
+        }
 
-		return contents.toString();
-	}
+        StringBuilder contents = new StringBuilder(DEFAULT_SIZE);
 
-	public static void main(String[] args) throws Exception {
-		if (args.length < 1) {
-			System.out.println("MP1 <User ID>");
-		} else {
-			String userName = args[0];
-			String inputFileName = "./input.txt";
-			MP1 mp = new MP1(userName, inputFileName);
-			String[] topItems = mp.process();
-			for (String item : topItems) {
-				System.out.println(item);
-			}
-		}
-	}
+        try (InputStream in = Files.newInputStream(FileSystems.getDefault().getPath(file.getPath()));
+                BufferedReader reader = new BufferedReader(new InputStreamReader(in))) {
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                contents.append(line + "\n");
+            }
+        }
+
+        return contents.toString();
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("MP1 <User ID>");
+        } else {
+            String userName = args[0];
+            String inputFileName = "./input.txt";
+            MP1 mp = new MP1(userName, inputFileName);
+            String[] topItems = mp.process();
+            for (String item : topItems) {
+                System.out.println(item);
+            }
+        }
+    }
 }
